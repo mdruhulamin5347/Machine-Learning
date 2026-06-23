569ff5798a9cb96189c51218302859ea42c7eed9
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2026-06-24 00:34:18 +0600

    Add examples for using np.where and np.searchsorted in NumPy practice script

--- NumPy-Practice/01_arrays.py
+++ NumPy-Practice/01_arrays.py
@@ -113,6 +113,25 @@ reshapenewarr = reshapearr.reshape(4, 3)
 
 
 
-arr = np.array([1, 2, 3, 4, 5, 6])
-newarr = np.array_split(arr, 3)
-print(newarr)
+# arr = np.array([1, 2, 3, 4, 5, 6])
+# newarr = np.array_split(arr, 3)
+# print(newarr)
+
+
+
+
+arr = np.array([10, 14, 93, 41, 8, 7])
+x = np.where(arr%2 == 0)
+print(x)
+
+
+
+arr = np.array([6, 7, 8, 9 ])
+x = np.searchsorted(arr, 7, side='right')
+print(x)
+
+
+
+arr = np.array([1, 3, 5, 7])
+x = np.searchsorted(arr, [2, 4, 6])
+print(x)
