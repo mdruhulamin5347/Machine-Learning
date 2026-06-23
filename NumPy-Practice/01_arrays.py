a9cfb82dfd27f12ed73ce0e81ffc2b021ca43f0c
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2026-06-24 00:37:27 +0600

    Comment out sorting examples in NumPy practice script

--- NumPy-Practice/01_arrays.py
+++ NumPy-Practice/01_arrays.py
@@ -139,15 +139,15 @@ reshapenewarr = reshapearr.reshape(4, 3)
 
 
 
-arr = np.array([3, 2, 0, 1])
-print(np.sort(arr))
+# arr = np.array([3, 2, 0, 1])
+# print(np.sort(arr))
 
 
 
-arr = np.array(['banana', 'cherry', 'apple'])
-print(np.sort(arr))
+# arr = np.array(['banana', 'cherry', 'apple'])
+# print(np.sort(arr))
 
 
 
-arr = np.array([[3, 2, 4], [5, 0, 1]])
-print(np.sort(arr))
+# arr = np.array([[3, 2, 4], [5, 0, 1]])
+# print(np.sort(arr))
