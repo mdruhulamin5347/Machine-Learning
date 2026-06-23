a6403bec3d6251610715d0a08961167d79050ab5
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2026-06-23 23:24:31 +0600

    Refactor array iteration examples and update to use np.ndenumerate in NumPy practice script

--- NumPy-Practice/01_arrays.py
+++ NumPy-Practice/01_arrays.py
@@ -51,13 +51,11 @@ reshapenewarr = reshapearr.reshape(4, 3)
 
 
 # arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
-
 # for x in arr:
 #   print(x)
 
 
 # arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
-
 # for x in arr:
 #   for y in x:
 #     for z in y:
@@ -65,7 +63,23 @@ reshapenewarr = reshapearr.reshape(4, 3)
 
 
 
-arr = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+# arr = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+# for x in np.nditer(arr):
+#   print(x)
 
-for x in np.nditer(arr):
-  print(x)
+
+# arr = np.array([1, 2, 3])
+# for x in np.nditer(arr, flags=['buffered'], op_dtypes=['S']):
+#   print(x)
+
+
+
+# arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+# for x in np.nditer(arr[:, ::2]):
+#   print(x)
+
+
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+for idx, x in np.ndenumerate(arr):
+  print(idx, x)
