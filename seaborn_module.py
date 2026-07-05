74608d934c5d818d3a83279ec2eeb07dcb5f0317
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2026-07-06 03:06:23 +0600

    Add seaborn_module.py for data visualization with seaborn and matplotlib
    - Introduced a new module `seaborn_module.py` that includes basic data visualization examples.
    - Implemented a scatter plot using matplotlib for car age and speed data (commented out).
    - Added a kernel density estimate plot using seaborn's `displot`.

---
+++ seaborn_module.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+# #day two, the age and speed of 15 cars:
+# x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
+# y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
+# plt.scatter(x, y)
+
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot([0, 1, 2, 3, 4, 5], kind="kde")
+
+plt.show()
